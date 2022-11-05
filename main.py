f3f54a895b4aa130509b4a650baef427bd336aaf
Kirill Varnatsov <kirill.varnatsov@gmail.com>
2022-11-05 23:56:59 +0200

    fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # import naive, products, schedule, current_products
 import schedule, time
 
-schedule.every().day.at("10:00", "Europe/Tallinn").do(current_products.update_current_products
+schedule.every().day.at("10:00", "Europe/Tallinn").do(current_products.update_current_products)
 
 schedule.every(3).days.at("10:00", "Europe/Tallinn").do(naive.run)
 schedule.every(3).days.at("10:00", "Europe/Tallinn").do(products.run)
