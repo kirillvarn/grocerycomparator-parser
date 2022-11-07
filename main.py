fe8394c45f87832c77fafbb8aee7aa5e31249da3
Kirill Varnatsov <kirill.varnatsov@gmail.com>
2022-11-07 13:08:06 +0200

    test

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import naive, products, current_products
 import schedule, time, datetime
 
-schedule.every().day.at("10:00").do(current_products.update_current_products)
 
-schedule.every(3).days.at("10:00").do(naive.run)
-schedule.every(3).days.at("10:00").do(products.run)
+current_products.update_current_products()
+naive.run()
+products.run()
 
-while True:
-    n = schedule.idle_seconds()
-    print(f"Starting schedule in {datetime.timedelta(seconds=n)}")
-    time.sleep(n)
-    schedule.run_pending()
+# schedule.every().day.at("10:00").do(current_products.update_current_products)
+
+# schedule.every(3).days.at("10:00").do(naive.run)
+# schedule.every(3).days.at("10:00").do(products.run)
+
+# while True:
+#     n = schedule.idle_seconds()
+#     print(f"Starting schedule in {datetime.timedelta(seconds=n)}")
+#     time.sleep(n)
+#     schedule.run_pending()
