2afcc7f2fa5bc47cbd2cb563713a60cd8662c8f2
Kirill Varnatsov <kirill@globalreader.eu>
2023-05-14 14:46:35 +0300

    fix

--- main.py
+++ main.py
@@ -6,21 +6,6 @@ from db import log_products
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
 
-
-now_dt = datetime.now()
-next_4am = None
-
-if now_dt.hour < 4 and now_dt.hour > 0:
-    next_4am = datetime(now_dt.year, now_dt.month, now_dt.day, 4)
-else:
-    time_d = timedelta(days=1)
-    next_4am = datetime(now_dt.year, now_dt.month, now_dt.day, 4) + time_d
-
-sched = BackgroundScheduler(daemon=True)
-sched.add_job(run,'interval', start_date=next_4am, days=1)
-sched.add_listener(job_init_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
-sched.start()
-
 def job_init_listener(event):
     if event.exception:
         print(f"Failed. Stacktrace: {event.exception}")
@@ -41,3 +26,18 @@ def run():
         i.join()
 
     log_products()
+
+
+now_dt = datetime.now()
+next_4am = None
+
+if now_dt.hour < 4 and now_dt.hour > 0:
+    next_4am = datetime(now_dt.year, now_dt.month, now_dt.day, 4)
+else:
+    time_d = timedelta(days=1)
+    next_4am = datetime(now_dt.year, now_dt.month, now_dt.day, 4) + time_d
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(run, 'interval', start_date=next_4am, days=1)
+sched.add_listener(job_init_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
+sched.start()
